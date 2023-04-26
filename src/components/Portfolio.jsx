7bb0b46c6c9f50e1085de016584c9fa25095befc
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-26 18:17:37 -0400

    asd

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,26 +1,40 @@
 import React, { useState } from 'react';
-import { Modal } from 'react-bootstrap';
+import { Modal, Button } from 'react-bootstrap';
 import './styles/Portfolio.css';
 
 const Portfolio = () => {
   const portfolioItems = [
     {
       id: 1,
-      imgSrc: 'https://unsplash.imgix.net/uploads%2F1411419068566071cef10%2F7562527b?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=240c45655f09c546232a6f106688e502',
+      imgSrc: '/images/c1.png',
       title: 'Round Icons',
       subtitle: 'Graphic Design',
+      link: 'https://inquisitive-quokka-67b163.netlify.app/',
+      description: (
+        <>
+          - Una aplicación desarrollada en JavaScript y React que muestra al azar citas motivadoras de personajes históricos y autores famosos.
+          <br />
+          - Diseño minimalista, fácil de usar, Interfaz limpia y atractiva. ideal para aquellos que buscan inspiración rápida o desean explorar la programación en React.
+          <br />
+          - Base de datos con citas variadas de frases célebres de figuras importantes en diferentes campos, ofreciendo una amplia gama de pensamientos y perspectivas inspiradoras.
+        </>
+      ),
     },
     {
       id: 2,
       imgSrc: 'https://unsplash.imgix.net/44/9s1lvXLlSbCX5l3ZaYWP_hdr-1.jpg?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=f0a1db79752dbb04ec6d2aab7d17c7b0',
       title: 'Startup Framework',
       subtitle: 'Website Design',
+      link: 'https://inquisitive-quokka-67b163.netlify.app/',
+      description: 'asd',
     },
     {
       id: 3,
       imgSrc: 'https://unsplash.imgix.net/44/9s1lvXLlSbCX5l3ZaYWP_hdr-1.jpg?q=75&w=1080&h=1080&fit=max&fm=jpg&auto=format&s=f0a1db79752dbb04ec6d2aab7d17c7b0',
       title: 'Startup Framework',
       subtitle: 'Website Design',
+      link: 'https://inquisitive-quok3ka-67b163.netlify.app/',
+      description: 'asd',
     },
   ];
 
@@ -42,46 +56,53 @@ const Portfolio = () => {
         <div className="row">
           {portfolioItems.map(item => (
             <div key={item.id} className="col-md-4 col-sm-6 portfolio-item">
-              <div
-                className="portfolio-link"
-                onClick={() => handleItemClick(item)}
-              >
-                <div className="portfolio-hover">
-                  <div className="portfolio-hover-content">
-                    <i className="fa fa-plus fa-3x"></i>
-                  </div>
-                </div>
-                <img src={item.imgSrc} className="img-responsive" alt="" />
-              </div>
-              <div className="portfolio-caption">
-                <h4>{item.title}</h4>
-                <p className="text-muted">{item.subtitle}</p>
-              </div>
-            </div>
-          ))}
+                      <div
+      className="portfolio-link"
+      onClick={() => handleItemClick(item)}
+    >
+      <div className="portfolio-hover">
+        <div className="portfolio-hover-content">
+          <i className="fa fa-plus fa-3x"></i>
         </div>
       </div>
-
-      {selectedItem && (
-        <Modal show={showModal} onHide={handleCloseModal} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>{selectedItem.title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="modal-img-container">
-            <img src={selectedItem.imgSrc} className="img-responsive modal-img" alt="" />
+      <img src={item.imgSrc} className="img-responsive" alt="" />
+    </div>
+          <div className="portfolio-caption">
+            <h4>{item.title}</h4>
+            <p className="text-muted">{item.subtitle}</p>
           </div>
-          <p>{selectedItem.subtitle}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <button onClick={handleCloseModal} className="btn btn-primary">
-            Cerrar
-          </button>
-        </Modal.Footer>
-      </Modal>
-      )}
-    </section>
-  );
+        </div>
+      ))}
+    </div>
+  </div>
+
+  {selectedItem && (
+     <Modal show={showModal} onHide={handleCloseModal} centered>
+     <Modal.Header closeButton>
+       <Modal.Title>{selectedItem.title}</Modal.Title>
+     </Modal.Header>
+     <Modal.Body>
+       <div className="modal-img-container">
+         <img src={selectedItem.imgSrc} className="img-responsive modal-img" alt="" />
+       </div>
+       <p>{selectedItem.subtitle}</p>
+       <p>{selectedItem.description}</p>
+     </Modal.Body>
+     <Modal.Footer>
+       <Button
+         variant="success"
+         onClick={() => window.open(selectedItem.link, "_blank")}
+       >
+         Ir a la página
+       </Button>
+       <Button variant="primary" onClick={handleCloseModal}>
+         Cerrar
+       </Button>
+     </Modal.Footer>
+   </Modal>
+  )}
+</section>
+);
 };
 
 export default Portfolio;
